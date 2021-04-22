f1c67ae6e9655c1db992c5b7a20abe99bc39acbb
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-04-22 11:41:45 +0600

    Add files via upload

---
+++ Average of 5 number/Untitled2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num_1,num_2,num_3,num_4,num_5;
+    cout<<"enter five number one by one : ";
+    cin>>num_1>>num_2>>num_3>>num_4>>num_5;
+    float ave= (num_1+num_2+num_3+num_4+num_5)/5;
+    cout<<"average is : "<<ave<<endl;
+    return 0;
+}
